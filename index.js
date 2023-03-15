5f554df1da4af110913b141237dd9ac6c5df2fc0
Ryan Rizeq <ryanrizeq@gmail.com>
2023-03-14 18:28:23 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 2;
+const num2 = 31;
+const num3 = 24;
+const num4 = 5;
 
-// Write your code here
+
+
+let multiply = num1 * num2;
+let random = Math.round(Math.random() * 10);
+let mod = num3 % num4;
+let max = Math.max(2, 20);
